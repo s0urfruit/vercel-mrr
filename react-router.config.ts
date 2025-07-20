42d6b627e6a64281f2a5e92b96d6b93a7fbebfc3
Sourfruit <hello@def-not-hacking-the.net>
2025-07-20 00:35:02 -0400

    Removed vercelPreset() for demo purposes

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,5 @@ import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
-    presets: [vercelPreset()],
+    // presets: [vercelPreset()],
 } satisfies Config;
