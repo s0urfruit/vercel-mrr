189360da0acacbb5f297cacba34a10c8d422f797
Sourfruit <hello@def-not-hacking-the.net>
2025-07-20 02:18:20 -0400

    reReturn vercelpreset 🎉

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,6 @@ import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
-    // presets: [vercelPreset()],
+    presets: [vercelPreset()],
+    ssr: true,
 } satisfies Config;
